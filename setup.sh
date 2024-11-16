937b8020944361a726b065b8cf025e1068ba3076
Maxime Sahuc <maxime.sahuc76@gmail.com>
2024-11-17 00:56:03 +0100

    edit setup script

--- setup.sh
+++ setup.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
 # rename old bashrc file
-mv ~/.bashrc ~/.bashrc.old &>/dev/null
+mv ~/.bashrc ~/.bashrc.old &>/dev/null && echo "Move .bashrc to .bashrc.old"
 
 # create .bashrc symlink
+echo "Creating new .bashrc synlink"
 ln -s ~/.custom/bashrc ~/.bashrc
+
+echo "Done!"
