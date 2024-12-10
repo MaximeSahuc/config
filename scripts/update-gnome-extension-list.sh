0cea09be50fec83ecf504a4d6f1fca013f317955
Maxime Sahuc <maxime.sahuc76@gmail.com>
2024-12-10 02:06:00 +0100

    first commit

---
+++ scripts/update-gnome-extension-list.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+gnome-extensions list > ../installs/gnome-extensions
+
---
+++ setup.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# rename old bashrc file
+mv ~/.bashrc ~/.bashrc.old &>/dev/null && echo "Move .bashrc to .bashrc.old"
+
+# create .bashrc symlink
+echo "Creating new .bashrc synlink"
+ln -s ~/.custom/bashrc ~/.bashrc
+
+# create dotfiles symlinks
+for file in dotfiles/*; do
+  if [ -f "$file" ]; then
+    fullsourcepath="$(pwd)/$file"
+    filename="$(basename $file)"
+    fulldestpath="$HOME/.$filename"
+
+    echo "create symlink for .$filename"
+    mv $fulldestpath "$fulldestpath.old" &>/dev/null && echo ".$filename already exists, renaming it to .$filename.old"
+    ln -s $fullsourcepath $fulldestpath
+  fi
+done
+
+echo "Done!"
